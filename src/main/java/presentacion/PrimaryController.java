c07e02b7ac37096d605eeeff3e15a360a6de8f41
=Albert Montero <=montero.villar.albert@alumnat.copernic.cat>
2023-01-27 13:16:23 +0100

    Interfaz de Usuario Provisional creada.

--- src/main/java/presentacion/PrimaryController.java
+++ src/main/java/presentacion/PrimaryController.java
@@ -1,12 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package presentacion;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class PrimaryController {
+
+/**
+ *
+ * @author Albert
+ */
+public class PrimaryController implements Initializable {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Button btnSearch;
+
+    @FXML
+    private Button btnExport;
+
+    @FXML
+    private ToggleButton btnSwitch;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    private Button btnDoc;
+
+    @FXML
+    private Button btnImport;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
     }
+    
 }
+
